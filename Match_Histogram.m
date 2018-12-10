3caaf5edebc332cd56926fd1166d6a5c40ab15de
thumbe3 <43432401+thumbe3@users.noreply.github.com>
2018-12-10 13:45:43 -0600

    Create Match_Histogram.m

---
+++ Match_Histogram.m
@@ -0,0 +1,35 @@
+content=imread('content.jpg');
+style=imread('style.jpg');
+content=imresize(content,[512,512]);
+style=imresize(style,[512,512]);
+figure;
+imshow(content);
+
+meanC=double([mean(mean(content(:,:,1)));mean(mean(content(:,:,2)));mean(mean(content(:,:,3)))]);
+meanS=double([mean(mean(style(:,:,1)));mean(mean(style(:,:,2)));mean(mean(style(:,:,3)))]);
+
+sigmaC=zeros(3,3);
+sigmaS=zeros(3,3);
+N=size(content,1)*size(content,2);
+
+for i=1:size(content,1)
+    for j=1:size(content,2)
+        xC=double(reshape(content(i,j,:),[3,1]));
+        xS=double(reshape(style(i,j,:),[3,1]));
+        sigmaC=sigmaC+(xC-meanC)*(xC-meanC)'/N;
+        sigmaS=sigmaS+(xS-meanS)*(xS-meanS)'/N;
+    end
+end
+
+A=sqrtm(sigmaC)*inv(sqrtm(sigmaS))
+b=meanC-A*meanS
+
+styleNew=zeros(size(style,1),size(style,2),3);
+for i=1:size(style,1)
+    for j=1:size(style,2)
+    new=A*reshape(double(style(i,j,:)),[3,1])+b;
+    styleNew(i,j,:)=reshape(new,[1,1,3]);
+    end
+end
+figure;
+imshow(uint8(styleNew));
